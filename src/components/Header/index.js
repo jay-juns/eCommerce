bf0eee5fb1811c3a0db502e23176e7b150c58fe0
jay-juns <necrozerg2@naver.com>
2020-10-24 09:45:52 +0900

    add google login

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './styles.scss';
 import { Link } from 'react-router-dom';
+import { auth } from './../../firebase/utils';
+
 import Logo from './../../assets/logo.png';
 
 const Header = props => {
+  const { currentUser } = props;
+
   return (
     <header className="header">
       <div className="wrap">
@@ -12,19 +16,43 @@ const Header = props => {
            <img src={Logo} alt="LOGO" />
           </Link>
         </div>
-
-        <div className="callToActions">
-          <ul>
-            <li>
-              <Link to="/registration">
-                Register
-              </Link>
-            </li>
-          </ul>
-        </div>
+        
+        {currentUser && (
+          <div className="callToActions">
+            <ul>
+              <li>
+                <span onClick={() => auth.signOut()}>
+                  LogOut
+                </span>
+              </li>
+            </ul>
+          </div>
+        )}
+        
+        {!currentUser && (
+          <div className="callToActions">
+            <ul>
+              <li>
+                <Link to="/registration">
+                  Register
+                </Link>
+              </li>
+              <li>
+                <Link to="/login">
+                  Login
+                </Link>
+              </li>
+            </ul>
+          </div>
+        )}
+        
       </div>
     </header>
   );
+};
+
+Header.defaultProps = {
+  currentUser: null
 };
 
 export default Header;
---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDDXOooF32jp_n90rBip4g49Q-f9KsQX_Y",
+  authDomain: "wow-ecommerce-web.firebaseapp.com",
+  databaseURL: "https://wow-ecommerce-web.firebaseio.com",
+  projectId: "wow-ecommerce-web",
+  storageBucket: "wow-ecommerce-web.appspot.com",
+  messagingSenderId: "300579834274",
+  appId: "1:300579834274:web:132fb22fc0d72be127cafb"
+};
